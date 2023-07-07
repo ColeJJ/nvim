e28e31c8034612ef93b5002a4d7c51fa2d364772
ColeJJ <torben.unland@gmail.com>
2023-07-07 23:06:16 +0200

    reordered dap config and added some icons

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -2,16 +2,9 @@ local dap = require("dap")
 local dapui = require("dapui")
 local dapGo = require("dap-go")
 
--- dab ui
--- dap.listeners.after.event_initialized["dapui_config"] = function()
---   dapui.open()
--- end
--- dap.listeners.before.event_terminated["dapui_config"] = function()
---   dapui.close()
--- end
--- dap.listeners.before.event_exited["dapui_config"] = function()
---   dapui.close()
--- end
+-- set icons
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
 
 -- PHP
 dap.adapters.php = {
@@ -38,3 +31,54 @@ dapGo.setup()
 
 -- maps
 require("colejj.debugmaps")
+
+-- DAP UI 
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- require("dapui").setup({
+--   icons = { expanded = "▾", collapsed = "▸" },
+--   mappings = {
+--     -- Use a table to apply multiple mappings
+--     expand = { "<CR>", "<2-LeftMouse>" },
+--     open = "o",
+--     remove = "d",
+--     edit = "e",
+--     repl = "r",
+--   },
+--   sidebar = {
+--     -- You can change the order of elements in the sidebar
+--     elements = {
+--       -- Provide as ID strings or tables with "id" and "size" keys
+--       {
+--         id = "scopes",
+--         size = 0.25, -- Can be float or integer > 1
+--       },
+--       { id = "breakpoints", size = 0.25 },
+--       { id = "stacks", size = 0.25 },
+--       { id = "watches", size = 00.25 },
+--     },
+--     size = 40,
+--     position = "left", -- Can be "left", "right", "top", "bottom"
+--   },
+--   tray = {
+--     elements = { "repl" },
+--     size = 10,
+--     position = "bottom", -- Can be "left", "right", "top", "bottom"
+--   },
+--   floating = {
+--     max_height = nil, -- These can be integers or a float between 0 and 1.
+--     max_width = nil, -- Floats will be treated as percentage of your screen.
+--     border = "single", -- Border style. Can be "single", "double" or "rounded"
+--     mappings = {
+--       close = { "q", "<Esc>" },
+--     },
+--   },
+--   windows = { indent = 1 },
+-- })
