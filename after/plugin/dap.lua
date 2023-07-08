d79288b71045a8f3c34d698e5b081dd0b3190b5d
ColeJJ <torben.unland@gmail.com>
2023-07-08 16:00:21 +0200

    configured dap xdebug debugging for php

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -6,19 +6,24 @@ local dapGo = require("dap-go")
 dap.adapters.php = {
   type = 'executable',
   command = 'node',
-  args = { os.getenv("HOME") .. '/.vscode-server/extensions/xdebug.php-debug-1.32.1/out/phpDebug.js' }
+  -- TODO: path for wsl different 
+  args = { os.getenv("HOME") .. '/.config/nvim/config_folders/vscode-php-debug/out/phpDebug.js' }
 }
 
 dap.configurations.php = {
   {
+    type = "php",
+    request = "launch",
+    name = "Listen for Xdebug",
+    port = 9003,
+  },
+  {
     type = 'php',
     request = 'launch',
-    name = 'Listen for Xdebug',
-    port = 9000,
-    -- pathMappings = {
-    --     ["/var/www/html"] = "${workspaceFolder}"
-    --     -- { localRoot = '${workspaceRoot}', remoteRoot = '.' }
-    -- }
+    name = 'Launch currently open script',
+    program = '${file}',
+    cwd = '${fileDirname}',
+    port = 9003
   }
 }
 
