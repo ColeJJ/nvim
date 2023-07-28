a4144c5cc3ca824e7dd91bccd5fe80c21d13ed8d
ColeJJ <torben.unland@gmail.com>
2023-07-28 13:23:01 +0200

    added keymaps to git fugitive and removed plugin from gitignore

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);  
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gv", vim.cmd.Gvsplit);
+vim.keymap.set("n", "<leader>gl", ":Git log <CR>");
+vim.keymap.set("n", "<leader>gb", ":Git blame <CR>");
