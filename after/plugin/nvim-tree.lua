480842867f0b0ad471752ce7ec3f2fbc71792cc0
ColeJJ <torben.unland@gmail.com>
2023-08-01 17:38:25 +0200

    not automatically showing the nerd tree now

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -12,7 +12,7 @@ nvim_tree.setup({
     hijack_cursor = true,
     hijack_unnamed_buffer_when_opening = false,
     hijack_directories = {
-        enable = true,
+        enable = false,
     },
     sync_root_with_cwd = true,
     update_focused_file = {
