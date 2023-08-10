0b39352bdd4f22d8d0410769e55827042e529d86
torben.unland <torben.unland@gsits.de>
2023-08-10 08:34:35 +0200

    remaped dap continue

--- lua/colejj/debugmaps.lua
+++ lua/colejj/debugmaps.lua
@@ -3,7 +3,7 @@ local dap = require('dap')
 
 -- setup debug
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
