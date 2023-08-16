76b27d7a2efa63696fbe8046c5e40247d43b59a5
torben.unland <torben.unland@gsits.de>
2023-08-16 11:40:45 +0200

    restructured neovim set

--- lua/colejj/set.lua
+++ lua/colejj/set.lua
@@ -1,18 +1,10 @@
--- vim.opt.guicursor = ""
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false 
+vim.opt.wrap = false
 
 vim.opt.swapfile = false 
 vim.opt.backup = false 
@@ -36,7 +28,26 @@ vim.opt.colorcolumn = ""
 
 vim.g.fillchars='eob: '
 
-
 -- highlights
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
+
+-- FORMATTING
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Format: HTML
+vim.g.html_indent_style1 = "auto"
+vim.g.html_indent_script1 = "auto"
+
+-- Automatically format the current line after leaving Insert mode
+-- vim.cmd([[
+-- augroup FormatOnLeave
+--     autocmd!
+--     autocmd InsertLeave * normal ==O
+-- augroup END
+-- ]])
