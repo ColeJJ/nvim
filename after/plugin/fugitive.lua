babee83bd4e8ff5bdc2bda2e7baec72dca4652d5
torben.unland <torben.unland@gsits.de>
2023-08-16 14:23:22 +0200

    added fugitive diff highlighting, added git diff shortcut and changed live grep shortcut

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gv", vim.cmd.Gvsplit);
+vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit);
 vim.keymap.set("n", "<leader>gl", ":Git log <CR>");
 vim.keymap.set("n", "<leader>gb", ":Git blame <CR>");
+
+
+-- add diff highlighting colors
+vim.cmd([[hi DiffAdd guifg=NONE guibg=#0f7d2d]])
+vim.cmd([[hi DiffDelete guifg=NONE guibg=#7d160f]])
