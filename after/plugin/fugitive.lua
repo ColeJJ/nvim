31c31078139efbb1ed7e63460b04df3d478dcbf2
torben.unland <torben.unland@gsits.de>
2023-10-13 13:15:04 +0200

    added git rebase keymap and changed git blame keymap

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,7 +2,8 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gv", vim.cmd.Gvsplit);
 vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit);
 vim.keymap.set("n", "<leader>gl", ":Git log <CR>");
-vim.keymap.set("n", "<leader>gb", ":Git blame <CR>");
+vim.keymap.set("n", "<leader>gb", ":Git rebase -i HEAD~");
+vim.keymap.set("n", "<leader>gc", ":Git blame <CR>");
 
 
 -- add diff highlighting colors
