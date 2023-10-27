c39bb254342ae3c5a500c5f90dee3ec5c2638b25
ColeJJ <torben.unland@gmail.com>
2023-10-27 21:44:00 +0200

    removed smart case from telescope ripgrep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -12,7 +12,6 @@ telescope.setup({
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
-            case_mode = "smart_case",
         },
     },
 })
