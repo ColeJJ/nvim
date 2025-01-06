a0a590425b8db2d1c8df9715d6cae459000ddb7c
ColeJJ <torben.unland@gmail.com>
2025-01-06 14:36:45 +0100

    removed monorepo from telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,5 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
-local monorepo = require("monorepo")
 
 telescope.setup({
     defaults = {
@@ -26,15 +25,7 @@ telescope.setup({
     },
 })
 
--- monorepo
-monorepo.setup({
-    silent = true,
-    autoload_telescope = true,
-    data_path = vim.fn.stdpath("data")
-})
-
 telescope.load_extension('fzf')
-telescope.load_extension('monorepo')
 telescope.load_extension("git_worktree")
 
 -- mappings
@@ -50,9 +41,6 @@ vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { de
 vim.keymap.set('n', '<leader>fo', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>gr',function() builtin.live_grep() end, ns)
-vim.keymap.set('n', '<leader>me',function() telescope.extensions.monorepo.monorepo() end, ns)
-vim.keymap.set('n', '<leader>ma',function() monorepo.add_project() end, ns)
-vim.keymap.set('n', '<leader>md',function() monorepo.remove_project() end, ns)
 vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 -- tmux
